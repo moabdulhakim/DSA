a98fd4f7817eef6752b67d57f977a9681ccc5995
Mohammad Abdulhakim <duxwork0@gmail.com>
2026-02-11 13:56:24 +0200

    Implement initial vector and insertion sort

---
+++ src/algorithms/Sort.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+class Sort {
+public:
+	template<typename T>
+	static void insertionRecursive(T* start, T* end) {
+		if (start != end) {
+			insertionSort(start, end - 1);
+		}
+		else return;
+
+		T* current = end;
+		T* head = end - 1;
+		while (*head > *current) {
+			swap(*head, *current);
+
+			current--;
+			head--;
+		}
+	}
+
+	template<typename T>
+	static void insertion(T* start, T* end) {
+		if (start == nullptr || start >= end) return;
+
+		T* head = start + 1;
+		while (head < end) {
+			T* preHead = head;
+			T currentVal = *head;
+
+			T* virtualHead = head - 1;
+			while (currentVal < *virtualHead) {
+				*head = *virtualHead;
+
+				head--;
+				virtualHead--;
+			}
+
+			*head = currentVal;
+			head = preHead + 1;
+		}
+	}
+
+	template<typename T>
+	static void insertionAI(T* start, T* end) {
+		if (start == nullptr || start >= end) return;
+
+		for (T* head = start + 1; head <= end; ++head) {
+			T currentVal = *head;
+			T* j = head;
+
+			while (j > start && currentVal < *(j - 1)) {
+				*j = *(j - 1);
+				j--;
+			}
+			*j = currentVal;
+		}
+	}
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "./containers/Vector.cpp"
+using namespace std;
+
+int main() {
+	Vector<int> vec{ 1, 2, 3, 4, 5 };
+
+
+    return 0;
+}
